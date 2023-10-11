e939c9ea84f74e705eb2a31b1dda8510b6c03d18
yahyaalx <colo9irch@gmail.com>
2023-10-12 00:38:36 +0100

    eroor fix

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-        return len(a_dictionary.keys())
+    n = 0
+    key_ = list(a_dictionary.keys())
+
+    for i in key_:
+        n = n + 1
+
+    return (n)
