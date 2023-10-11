5e0b1b18b5fe5c4401f661bead22b5bfd3c0bf85
yahyaalx <colo9irch@gmail.com>
2023-10-11 15:59:53 +0100

    add dot in empty case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import argparse
     args = len(sys.argv) - 1
     if args == 0:
-        print("{:d} arguments".format(args))
+        print("{:d} arguments.".format(args))
     elif args == 1:
         print("{:d} argument:".format(args))
     else:
